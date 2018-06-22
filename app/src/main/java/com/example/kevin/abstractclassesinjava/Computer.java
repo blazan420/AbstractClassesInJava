8ed54daa7d85ddd2b6765d8141cf9a0f94bc33c4
kevin <kevinjobrien420@gmail.com>
2018-06-22 03:11:03 -0500

    229 App Number 28 - Part2- Abstract Classes in Java // Coding

--- app/src/main/java/com/example/kevin/abstractclassesinjava/Computer.java
+++ app/src/main/java/com/example/kevin/abstractclassesinjava/Computer.java
@@ -12,10 +12,33 @@ public abstract class Computer {
     // Constructor
     public Computer(String name, String screen, String keyboard) {
 
-        this.name = name;
+         this.name = name;
          this.screen = screen;
          this.keyboard = keyboard;
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getScreen() {
+        return screen;
+    }
+
+    public String getKeyboard() {
+        return keyboard;
+    }
+
+    @Override
+    public String toString() {
+
+
+        return String.format("%s%n %s%n %s", getName(), getScreen(), getKeyboard());
+
+
+
+    }
+
+    public abstract double evaluatePerformance();
 }
