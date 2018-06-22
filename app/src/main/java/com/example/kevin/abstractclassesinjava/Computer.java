b642b4cfb6af4aff17842bad31de2f8937777330
kevin <kevinjobrien420@gmail.com>
2018-06-22 02:45:22 -0500

    228 App Number 28 - Part1 - AbstractClassesIn Java // Coding

---
+++ app/src/main/java/com/example/kevin/abstractclassesinjava/Computer.java
@@ -0,0 +1,21 @@
+package com.example.kevin.abstractclassesinjava;
+
+public abstract class Computer {
+
+
+
+    private final String name;
+    private final String screen;
+    private final String keyboard;
+
+
+    // Constructor
+    public Computer(String name, String screen, String keyboard) {
+
+        this.name = name;
+         this.screen = screen;
+         this.keyboard = keyboard;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/kevin/abstractclassesinjava/TabletComputer.java
@@ -0,0 +1,4 @@
+package com.example.kevin.abstractclassesinjava;
+
+public class TabletComputer {
+}
